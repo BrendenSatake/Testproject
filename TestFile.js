2397221ca63745828ab101e04aab5009520f9e78
Brenden <BSatake@ucsc.edu>
2019-05-08 22:44:36 -0700

    Added something to conflict with

--- TestFile.js
+++ TestFile.js
@@ -3,3 +3,7 @@
 // Hello, I'm creating this file as my initial commit.
 
 // This comment was added in the FirstBranch branch
+
+// Time to create a conflict!
+var foo = "Rubber";
+var bar = "Duck";
