97eff5c62fadd1c33f21bd678d954ae1f660ae76
Brenden <BSatake@ucsc.edu>
2019-05-08 22:46:00 -0700

    Added a conflict, I hope

--- TestFile.js
+++ TestFile.js
@@ -3,3 +3,7 @@
 // Hello, I'm creating this file as my initial commit.
 
 // This comment was added in the FirstBranch branch
+
+// Time to create a conflict!
+var foo = "Hello World";
+var bar = "Goodbye World";
